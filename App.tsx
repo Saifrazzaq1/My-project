2f6c0cfca51dfbfc720674b4df5b49f4ea3d4b58
Saifrazzaq1 <saifrazzaq23@gmail.com>
2024-02-03 02:31:08 -0800

    tamplet

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {LogBox, StatusBar} from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import Snackbar from 'src/Components/Snackbar';
+import Navigation from 'src/Navigation';
+import {persistor, store} from 'src/Redux/ConfigureStore';
+
+LogBox.ignoreAllLogs();
+
+function App(): JSX.Element {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <StatusBar
+          backgroundColor={'transparent'}
+          barStyle="dark-content"
+          translucent
+        />
+        <Navigation />
+        <Snackbar />
+      </PersistGate>
+    </Provider>
+  );
+}
+
+export default App;
